428dd3b1ed6ff002218f49ac4df12d06a9a538b2
liv-it <liviatjam8@gmail.com>
2026-06-28 22:00:17 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ const line2 = document.getElementById("line2");
 const button = document.getElementById("continueBtn");
 const transition = document.getElementById("transition");
 
-function sleep(ms) {
+function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function type(element, text, speed = 60) {
+async function type(element,text,speed){
 
     element.classList.add("cursor");
 
-    for (let i = 0; i < text.length; i++) {
+    for(let i=0;i<text.length;i++){
 
         element.innerHTML += text.charAt(i);
 
@@ -24,36 +24,36 @@ async function type(element, text, speed = 60) {
 
 }
 
-async function start() {
+async function start(){
 
-    await type(title, "Dear Ruben Sugianto,", 70);
-
-    await sleep(700);
-
-    await type(line1, "Before you go...", 60);
+    await type(title,"Dear Ruben Sugianto,",70);
 
     await sleep(700);
 
-    await type(line2, "Can you guess what's waiting inside? 👀", 45);
+    await type(line1,"Before you go...",60);
+
+    await sleep(700);
+
+    await type(line2,"Can you guess what's waiting inside? 👀",45);
 
     await sleep(900);
 
-    button.style.display = "inline-block";
+    button.style.display="inline-block";
 
 }
 
 start();
 
-button.onclick = function (event) {
+button.onclick = function(event){
 
     event.preventDefault();
 
     transition.classList.add("show");
 
-    setTimeout(function () {
+    setTimeout(function(){
 
-        window.location.href = "letter.html";
+        window.location.href="letter.html";
 
-    }, 1200);
+    },1200);
 
 };
